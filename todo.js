ebd2eee7c28189f25fa1bac8925ee639ae8b38a3
hammadaslam1 <hammadaslam308@gmail.com>
2023-06-23 17:42:31 +0500

    updated

--- todo.js
+++ todo.js
@@ -2,14 +2,24 @@ var list = document.getElementById('event');
 
 const addEvent = () => {
    if (list.value!=='') {
+    const childList = document.createElement('div');
+    childList.className = 'list-child flex flex-row justify-between pr-3'; 
     const item = document.createElement('div');
-    const listChild = document.createElement('div');
-    item.className = 'items';
+    item.className = 'items ';
     item.style.padding = '10px';
-    item.style.borderBottom = '2px solid lightslategray';
+    childList.style.borderBottom = '2px solid lightslategray';
     var text = document.createTextNode(list.value);
+    document.getElementById("list").appendChild(childList);
+    childList.appendChild(item)
+    
+    const del = document.createElement('button');
+    del.className = 'btn';
     item.appendChild(text);
-    document.getElementById("list").appendChild(item);
+    childList.appendChild(del);
+    const icon1 = document.createElement('i');
+    icon1.className = "fa fa-trash";
+    del.appendChild(icon1);
+    
     const itemView  = document.getElementsByClassName('items');
      for (const i of Array.from(itemView)) {
         console.log(i.target);
@@ -21,3 +31,12 @@ const addEvent = () => {
    }
     list.value = '';
 }
+
+
+function deleteItem() {
+
+}
+
+/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link> */
+
+{/* <button class="btn"><i class="fa fa-trash"></i></button> */}
